8f05daf2645d0b4d4652431655219691704ec4ce
garciadiego <diegogarcia@MacBookPro.local>
2016-09-27 15:54:23 -0400

    Softmax Layer

--- QLearner_CartPole.py
+++ QLearner_CartPole.py
@@ -11,22 +11,19 @@ def softmax(x):
     ans = dg_exp_ / dg_exp_.sum()
     return out
 
-
-
-
 env = gym.make('CartPole-v0')
 for i_episode in range(20):
-    observation = env.reset()
+    observ = env.reset()
     total_reward=0
-    parameters=np.random.rand(4)*2-1
+    param =np.random.rand(4)*2-1
 
     for t in range(100):
         env.render()
-        print(observation)
+        print(observ)
 
-        action = 0 if np.matmul(parameters,observation) < 0 else 1
+        action = 0 if np.matmul(param,observ) < 0 else 1
 
-        observation, reward, done, info = env.step(action)
+        observ, reward, done, info = env.step(action)
         total_reward += reward
 
         print ("Total reward is %d"). format (t+1) % total_reward
